96082c120f14f3d0a2655bdd55c500e4eccfd518
hoangtc125 <tranconghoang2k1@gmail.com>
2023-04-22 12:10:53 +0700

    code: refactor; clustering: test real data

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+import json
+import os
+from os import getenv
+from dotenv import load_dotenv
+from pydantic import BaseSettings
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+
+class ProjectConfig(BaseSettings):
+    SERVICE_NAME = getenv("SERVICE_NAME", "ALGO_CLUSTERING")
+    STOPWORD_PATH = BASE_DIR +r'/resources/vn_stopword.txt'
+    FEATURES_PATH = BASE_DIR +r'/resources/features.txt'
+
+
+project_config = ProjectConfig()
+print("--- Algo Clustering Config:\n", json.dumps(project_config.dict(), indent=4))
---
+++ app/loader.py
@@ -0,0 +1,76 @@
+import gensim
+import torch
+import underthesea
+import numpy as np
+from typing import List
+from transformers import (
+    AutoModel,
+    AutoTokenizer,
+    T5ForConditionalGeneration,
+    T5Tokenizer,
+)
+from sklearn.preprocessing import OneHotEncoder
+from pandas.core.frame import DataFrame
+
+from app.core.config import project_config
+
+
+class Loader:
+    def __init__(self) -> None:
+        if torch.cuda.is_available():
+            self.__device = torch.device("cuda")
+
+            print("There are %d GPU(s) available." % torch.cuda.device_count())
+
+            print("We will use the GPU:", torch.cuda.get_device_name(0))
+        else:
+            print("No GPU available, using the CPU instead.")
+            self.__device = torch.device("cpu")
+
+        # Load model vector hóa
+        self.__phobert = AutoModel.from_pretrained("vinai/phobert-base").to(self.__device)
+        self.__tokenizer = AutoTokenizer.from_pretrained("vinai/phobert-base")
+
+        # # Load model summarization
+        # self.model = T5ForConditionalGeneration.from_pretrained("NlpHUST/t5-small-vi-summarization").to(device)
+        # self.t5tokenizer = T5Tokenizer.from_pretrained("NlpHUST/t5-small-vi-summarization")
+
+        self.__stop_word_data = np.genfromtxt(
+            project_config.STOPWORD_PATH, dtype="str", delimiter="\n", encoding="utf8"
+        ).tolist()
+        self.__one_hot_encoder = OneHotEncoder()
+
+    def feature_engineering(self, data: List):
+        features_set = []
+        for line in data:
+            line = gensim.utils.simple_preprocess(line)  # Tiền xử lý dữ liệu
+            line = " ".join(line)
+            line = underthesea.word_tokenize(line, format="text")  # Segment word
+            line = " ".join(
+                [word for word in line.split() if word not in self.__stop_word_data]
+            )
+            # if len(line.split()) > 100: # Summary long text
+            # self.model.eval()
+            #   tokenized_text = self.t5tokenizer.encode(line, return_tensors="pt").to(self.__device)
+            #   summary_ids = model.generate(
+            #                       tokenized_text,
+            #                       max_length=256,
+            #                       num_beams=5,
+            #                       repetition_penalty=2.5,
+            #                       length_penalty=1.0,
+            #                       early_stopping=True
+            #                   )
+            #   line = self.t5tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+            line = self.__tokenizer(line, truncation=True).input_ids  # tokenizer
+            input_ids = torch.tensor([line]).to(torch.long)
+            with torch.no_grad():  # Lấy features dầu ra từ BERT
+                features = self.__phobert(input_ids)
+            v_features = features[0][:, 0, :].numpy()
+            features_set.append(v_features[0])
+        features_set = np.array(features_set)
+        return features_set
+    
+    def one_hot_encoding(self, data: DataFrame):
+        return self.__one_hot_encoder.fit_transform(data).toarray()
+
+loader = Loader()
